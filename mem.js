44b92676897645bc7b6b254b54451157162fc94c
BOSSLADY <gennymiriane06@gmail,com>
2026-01-23 11:16:27 +0100

    Add README image

---
+++ mem.js
@@ -0,0 +1,23 @@
+# TECHNICAL-DOCUMENTATION
+
+(A simple overview/tagline of the project and its purpose.)
+
+![TECH-DOCUMENTAION Form Screenshot](assets/image/techdoc.png)
+
+## Description
+
+this is a js documentation that explains examples of JS 
+It helps you solve js problems and easily understaand JS
+
+## Installation
+
+.githut
+workflows
+linters.yml
+README.md
+index.html
+style.css
+
+## Author
+
+Mbopda Genny Miriane
